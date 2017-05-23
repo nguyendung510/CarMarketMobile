9ae935027987408a7c8e7262940859229dde7542
Harvey Nguyen <harvey.nguyen@starixsoft.com>
2017-05-23 17:55:58 +0700

    call ajax

--- CarMarketMobile/Scripts/Apps/index.js
+++ CarMarketMobile/Scripts/Apps/index.js
@@ -2,4 +2,25 @@
     $(".img-responsive").lazyload({
         effect: "fadeIn"
     });
+
+    $('input:radio').change(function (element) {
+        alert(this.value);
+    });
 });
+
+function SearchingCarsForYou() {
+    $.ajax({
+        url: '/Car/SearchingCarsForYou',
+        data: { category: $('#Category').val(), year: $('#Year').val(), status: $('#Status').val() === undefined ? -1 : $('#Status').val() },
+        method: "POST",
+        beforeSend: function () {
+        },
+        success: function (data) {
+            $('#carListContainer').html(data);
+        },
+        error: function () {
+        },
+        complete: function () {
+        }
+    });
+}
