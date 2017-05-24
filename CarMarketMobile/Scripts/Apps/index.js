0f9056223cdd47108b0e725ef4e54cd246d31de2
Harvey Nguyen <harvey.nguyen@starixsoft.com>
2017-05-24 17:44:56 +0700

    call ajax

--- CarMarketMobile/Scripts/Apps/index.js
+++ CarMarketMobile/Scripts/Apps/index.js
@@ -4,14 +4,49 @@
     });
 
     $('input:radio').change(function (element) {
-        alert(this.value);
+        debugger;
+        SearchingCarsForYou();
     });
 });
 
 function SearchingCarsForYou() {
     $.ajax({
         url: '/Car/SearchingCarsForYou',
-        data: { category: $('#Category').val(), year: $('#Year').val(), status: $('#Status').val() === undefined ? -1 : $('#Status').val() },
+        data: { CurrentPageIndex: 1, ItemsPerPage: 25 },
+        method: "POST",
+        beforeSend: function () {
+        },
+        success: function (data) {
+            $('#carListContainer').html(data);
+        },
+        error: function () {
+        },
+        complete: function () {
+        }
+    });
+}
+
+function SearchingCarsNewOld() {
+    $.ajax({
+        url: '/Car/SearchingCarsNewOld',
+        data: { CurrentPageIndex: 1, ItemsPerPage: 25 },
+        method: "POST",
+        beforeSend: function () {
+        },
+        success: function (data) {
+            $('#carListContainer').html(data);
+        },
+        error: function () {
+        },
+        complete: function () {
+        }
+    });
+}
+
+function SearchingCarsImportDomestic() {
+    $.ajax({
+        url: '/Car/SearchingCarsImportDomestic',
+        data: { CurrentPageIndex: 1, ItemsPerPage: 25 },
         method: "POST",
         beforeSend: function () {
         },
